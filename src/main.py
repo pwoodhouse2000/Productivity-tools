4bb06e875b165098ee134889da5641fc4f6416e2
Pete Woodhouse <pwoodhouse2000@users.noreply.github.com>
2025-06-15 09:20:41 +0100

    Set up project structure and add hello world function

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import functions_framework.http
+
+@functions_framework.http
+def sync_projects(request):
+    """Our first cloud function!"""
+    return {
+        'status': 'success'
+        'message': 'Hello from your Todoist-Notion syncapp!'
+    }
