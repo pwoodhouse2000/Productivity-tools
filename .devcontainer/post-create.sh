c628ee75305d113d964582fa2a5190ddac51d7b5
Pete Woodhouse <pwoodhouse2000@users.noreply.github.com>
2025-06-16 06:53:15 +0000

    Manual gcloud install

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+echo "--- Starting Manual gcloud Installation ---"
+# Download and install the Google Cloud SDK silently.
+curl -sSL https://sdk.cloud.google.com > install.sh
+bash install.sh --disable-prompts --install-dir=~/gcloud
+
+# Add gcloud to the PATH for the current and future terminal sessions.
+echo 'source ~/gcloud/google-cloud-sdk/path.bash.inc' >> ~/.bashrc
+source ~/gcloud/google-cloud-sdk/path.bash.inc
+
+# Clean up the installer
+rm install.sh
+echo "--- Manual gcloud Installation Complete ---"
+echo ""
 echo "--- Authenticating to Google Cloud ---"
 
 # Use the secret we stored in GitHub to log in.
@@ -11,8 +24,8 @@ gcloud config set project productivity-sync-463008
 
 # Clean up the key file for security.
 rm /tmp/gcp_key.json
-
 echo "--- Authentication complete. Project is set. ---"
+echo ""
 echo "--- Installing Python packages ---"
 
 # This command will install all packages listed in your requirements.txt file.
